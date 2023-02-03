2ff2fe8b35e2cace68fb0024972208d996cb6879
Emily Foglia <efoglia@ceiamerica.com>
2023-02-03 10:48:07 -0500

    Create file upload service and switch to using blob storage

---
+++ bootcamp-api.API/Exceptions/EmptyFileException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace bootcamp_api.Exceptions
+{
+    public class EmptyFileException : Exception
+    {
+        public override string Message => $"A non-empty file is required for file upload.";
+
+        public EmptyFileException() { }
+    }
+}
